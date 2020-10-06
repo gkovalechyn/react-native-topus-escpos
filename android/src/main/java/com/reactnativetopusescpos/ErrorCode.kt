d4fb88ec2f322ded2b1004941b135a964804c1fd
gkovalechyn <gkovalechyn@gmail.com>
2020-10-05 22:01:39 -0300

    First commit

---
+++ android/src/main/java/com/reactnativetopusescpos/ErrorCode.kt
@@ -0,0 +1,5 @@
+package com.reactnativetopusescpos;
+
+public enum class ErrorCode(val code: String) {
+  BLUETOOTH_NOT_AVAILABLE("bluetooth-not-available")
+}
--- android/src/main/java/com/reactnativetopusescpos/TopusEscposModule.kt
+++ android/src/main/java/com/reactnativetopusescpos/TopusEscposModule.kt
@@ -4,21 +4,44 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import android.bluetooth.BluetoothAdapter
 
 class TopusEscposModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "TopusEscpos"
+  override fun getName(): String {
+    return "TopusEscpos"
+  }
+
+
+  // Example method
+  // See https://facebook.github.io/react-native/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
+
+    promise.resolve(a * b)
+  }
+
+  @ReactMethod
+  fun isBluetoothEnabled(promise: Promise) {
+
+  }
+
+  @ReactMethod
+  fun findDevices(promise: Promise) {
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter();
+
+    if (bluetoothAdapter == null) {
+      promise.reject(ErrorCode.BLUETOOTH_NOT_AVAILABLE.code, "Bluetooth is not available for this device");
+      return;
     }
 
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
 
-    
+  }
+
+  @ReactMethod
+  fun enableBluetooth(promise: Promise) {
+
+  }
+
+
 }
